b9ae1635d144c7abfafdf2a15a77c9f3074e19c7
Mathias Hillesheim <mathiashls@protonmail.com>
2018-04-24 09:40:43 -0300

    Main changes on mm1 implementation

--- mm1.py
+++ mm1.py
@@ -9,15 +9,16 @@ from queue import Queue
 
 import numpy as np
 import matplotlib.pyplot as plt
+from math import log
 
 
 class Task(object):
 
-    def __init__(self):
-        self.arrivel_time = datetime.now()
+    def __init__(self, simulation_time):
+        self.simulation_time = simulation_time
 
     def get_time(self):
-        return self.arrivel_time
+        return self.simulation_time
 
 
 class NumberGen(object):
@@ -29,18 +30,24 @@ class NumberGen(object):
             self.rate = _mi
         else:
             raise
-        self._seed = random.randint(1,10000)
+        _seed = random.randint(1,10000)
+        self._random = random.Random()
+        self._random.seed(_seed)
+        self._audit = []
 
     def get_uniform(self):
-        _random = random.Random()
-        _random.seed(self._seed)
-        return _random.random()
+        uniform = self._random.random()
+        return uniform
 
     def get_exp(self):
         uniform = self.get_uniform()
         ln_uniform = - np.log(uniform)
         result = ln_uniform / self.rate
+        self._audit.append(result)
         return result
+
+    def audit(self):
+        return self._audit
 
 
 class MM1(object):
@@ -56,20 +63,35 @@ class MM1(object):
         self.state_audit = []
         self._queue = Queue()
 
-    def plot_result_array(self, array_to_plot, title="Unnamed", xlabel="X", ylabel="Y"):
+    def plot_result_array(self, array_to_plot, figure, title="Unnamed", xlabel="X", ylabel="Y", block=False):
+        plt.figure(figure)
         n, bins, patches = plt.hist(array_to_plot, 50, normed=1, facecolor='green', alpha=0.75)
 
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(title)
-        #plt.axis([40, 160, 0, 0.03])
         plt.grid(True)
 
-        plt.show()
+        plt.show(block=block)
+
+    def plot_queue_audit(self, queue_to_plot, figure, title="Unamed", xlabel="X", ylabel="Y", block=False):
+        #formated_list = [(time, log(qsize)) for time, qsize in queue_audit]
+        time_values = [time for (time, qsize) in queue_to_plot]
+        qsize_values = [qsize for (time, qsize) in queue_to_plot]
+        plt.figure(figure)
+        plt.plot(time_values, qsize_values)
+        plt.show(block=block)
 
     def set_state(self, state):
         self.state = state
         self.state_audit.append(state)
+
+    def update_statistics(self):
+        self.time_since_last_event = self.sim_time - self.last_event_time
+        self.cumulated_queue_len = self._queue.qsize() * self.time_since_last_event
+
+        if self.state == self.BUSY:
+            self.total_busy_time += self.time_since_last_event
 
     def run_simulation(self, _lambda, _mi, max_requests):
 
@@ -78,33 +100,31 @@ class MM1(object):
         rng1 = NumberGen(_lambda=_lambda)
         rng2 = NumberGen(_mi=_mi)
 
-        arrives = []
-        departures = []
-        all_tasks = []
-
-        simulation_requests = 0.0
+        self.sim_time = 0
+        self.last_event_time = 0
+        self.total_busy_time = 0
+        queue_audit = []
         next_departure = self.DEPARTURE_REFERENCE
         next_arrival = rng1.get_exp();
-        arrives.append(next_arrival)
 
-        while (simulation_requests < max_requests):
+        self.update_statistics()
+
+        while (self.sim_time < max_requests):
             if(next_arrival < next_departure):
                 print("New arrival!")
-                simulation_requests = next_arrival
+                self.sim_time = next_arrival
                 if (self.state == self.IDLE):
                     self.set_state(self.BUSY)
                     print("Queue is now BUSY!")
-                    next_departure = simulation_requests + rng2.get_exp()
-                    departures.append(next_departure)
+                    next_departure = self.sim_time + rng2.get_exp()
                 else:
-                    new_task = Task()
-                    all_tasks.append(new_task)
+                    new_task = Task(self.sim_time)
                     print("Inserting task on queue")
                     self._queue.put(new_task)
-                next_arrival = simulation_requests + rng1.get_exp()
-                arrives.append(next_arrival)
+                    queue_audit.append((self.sim_time, self._queue.qsize()))
+                next_arrival = self.sim_time + rng1.get_exp()
             else:
-                simulation_requests = next_departure
+                self.sim_time = next_departure
                 if (self._queue.empty()):
                     self.set_state(self.IDLE)
                     print("Queue is now IDLE!")
@@ -112,15 +132,23 @@ class MM1(object):
                 else:
                     self._queue.get()
                     print("Departuring task on queue")
-                    next_departure = simulation_requests + rng2.get_exp()
-                    departures.append(next_departure)
+                    next_departure = self.sim_time + rng2.get_exp()
+            self.last_event_time = self.sim_time
 
+        # Simulation total time
         simulation_end_time = datetime.now()
         simulation_total_time = simulation_end_time - simulation_start_time
         print("Simulation took %s to be completed." % simulation_total_time)
 
-        self.plot_result_array(departures)
-        self.plot_result_array(arrives)
+        avg_queue_length = self.cumulated_queue_len / self.sim_time;
+        avg_utilization = self.total_busy_time / self.sim_time;
+        print("The average queue lenght was %s and the average "
+              "utilization was %s" % (avg_queue_length, avg_utilization))
+
+        print(queue_audit)
+        self.plot_queue_audit(queue_audit, 0)
+        self.plot_result_array(rng1.audit(), 1, title="RNG1 Plot")
+        self.plot_result_array(rng2.audit(), 2, title="RNG2 Plot", block=True)
 
 def main():
     mm1 = MM1()
