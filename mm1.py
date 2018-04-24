f84c2ff798483c3ebbf6205ad978dfe7f2b7fa8f
Mathias Hillesheim <mathiashls@protonmail.com>
2018-04-24 01:23:06 -0300

    WIP - Trying to fix the plots

--- mm1.py
+++ mm1.py
@@ -1,7 +1,14 @@
+"""
+'This code is so ugly it hurts my eyes.'
+Mathias Hillesheim, this code's father
+"""
+
 import random
-import numpy as np
 from datetime import datetime
 from queue import Queue
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Task(object):
@@ -39,51 +46,85 @@ class NumberGen(object):
 class MM1(object):
 
 
-    def __init__(self, max_requests):
+    def __init__(self):
         print("Initing MM1 queue...")
-        self.IDLE = 'idle'
-        self.BUSY = 'busy'
+        self.IDLE = 0
+        self.BUSY = 1
         self.DEPARTURE_REFERENCE = random.getrandbits(128)
 
         self.state = self.IDLE
+        self.state_audit = []
         self._queue = Queue()
 
+    def plot_result_array(self, array_to_plot, title="Unnamed", xlabel="X", ylabel="Y"):
+        n, bins, patches = plt.hist(array_to_plot, 50, normed=1, facecolor='green', alpha=0.75)
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        #plt.axis([40, 160, 0, 0.03])
+        plt.grid(True)
+
+        plt.show()
+
+    def set_state(self, state):
+        self.state = state
+        self.state_audit.append(state)
+
     def run_simulation(self, _lambda, _mi, max_requests):
+
+        simulation_start_time = datetime.now()
 
         rng1 = NumberGen(_lambda=_lambda)
         rng2 = NumberGen(_mi=_mi)
 
-        simulation_time = 0.0
+        arrives = []
+        departures = []
+        all_tasks = []
+
+        simulation_requests = 0.0
         next_departure = self.DEPARTURE_REFERENCE
         next_arrival = rng1.get_exp();
+        arrives.append(next_arrival)
 
-        while (simulation_time < max_requests):
+        while (simulation_requests < max_requests):
             if(next_arrival < next_departure):
                 print("New arrival!")
-                simulation_time = next_arrival
+                simulation_requests = next_arrival
                 if (self.state == self.IDLE):
-                    self.state = self.BUSY
+                    self.set_state(self.BUSY)
                     print("Queue is now BUSY!")
-                    next_departure = simulation_time + rng2.get_exp()
+                    next_departure = simulation_requests + rng2.get_exp()
+                    departures.append(next_departure)
                 else:
                     new_task = Task()
+                    all_tasks.append(new_task)
                     print("Inserting task on queue")
                     self._queue.put(new_task)
-                next_arrival = simulation_time + rng1.get_exp()
+                next_arrival = simulation_requests + rng1.get_exp()
+                arrives.append(next_arrival)
             else:
-                simulation_time = next_departure
+                simulation_requests = next_departure
                 if (self._queue.empty()):
-                    self.state = self.IDLE
+                    self.set_state(self.IDLE)
                     print("Queue is now IDLE!")
                     next_departure = self.DEPARTURE_REFERENCE
                 else:
-                    self._queue.pop()
+                    self._queue.get()
                     print("Departuring task on queue")
-                    next_departure = simulation_time + rng2.get_exp()
+                    next_departure = simulation_requests + rng2.get_exp()
+                    departures.append(next_departure)
+
+        simulation_end_time = datetime.now()
+        simulation_total_time = simulation_end_time - simulation_start_time
+        print("Simulation took %s to be completed." % simulation_total_time)
+
+        self.plot_result_array(departures)
+        self.plot_result_array(arrives)
 
 def main():
-    mm1 = MM1(100)
-    mm1.run_simulation(1, 2, 100)
+    mm1 = MM1()
+    mm1.run_simulation(1, 2, 1000)
 
 if __name__ == '__main__':
     main()
